f9527aa4e5e32d1b5499bc332743b90d906fd595
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2024-02-09 10:26:22 +0300

    Update functions.cpp

--- Math/functions.cpp
+++ Math/functions.cpp
@@ -148,11 +148,3 @@ void sieve(int n) {
     }
 }
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-
-template<typename num_t>
-using ordered_set = tree<num_t, null_type, less<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
-
