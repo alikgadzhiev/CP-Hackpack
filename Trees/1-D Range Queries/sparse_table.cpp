5b59e05155c8965269a0ecf14441fd19468f3adc
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2024-04-03 23:25:25 +0300

    added function

--- Trees/1-D Range Queries/sparse_table.cpp
+++ Trees/1-D Range Queries/sparse_table.cpp
@@ -6,13 +6,11 @@ using namespace std;
 // can use to find maximum, minimum or gcd on queries
 // but only if the values of array do not change
 // for example, sum queries, etc.
-template <typename T>
+
+template <typename T, class F = function<T(const T&, const T&)>>
 struct sparse_table{
 	vector<vector<T>> ST;
-
-	T comb(T x, T y) {
-		return max(x, y);
-	}
+  	F func;
 
 	sparse_table(vector<T> &A){
 		int N = A.size();
@@ -23,15 +21,17 @@ struct sparse_table{
 		}
 		for (int i = 0; i < LOG - 1; i++){
 			for (int j = 0; j < N - (1 << i); j++){
-				ST[i + 1][j] = comb(ST[i][j], ST[i][j + (1 << i)]);
+				ST[i + 1][j] = func(ST[i][j], ST[i][j + (1 << i)]);
 			}
 		}
 	}
 	T query(int L, int R){ // [L, R)
 		int d = 31 - __builtin_clz(R - L);
-		return comb(ST[d][L], ST[d][R - (1 << d)]);
+		return func(ST[d][L], ST[d][R - (1 << d)]);
 	}
 };
+
+// link - https://codeforces.com/contest/1548/submission/229928645
 
 int main(){
 }
