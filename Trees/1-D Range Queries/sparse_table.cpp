2765986ab66963568e0967e5dc6e3f01afa22bcb
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2024-05-30 19:56:18 +0300

    Update sparse_table.cpp

--- Trees/1-D Range Queries/sparse_table.cpp
+++ Trees/1-D Range Queries/sparse_table.cpp
@@ -8,27 +8,30 @@ using namespace std;
 // for example, sum queries, etc.
 
 template <typename T, class F = function<T(const T&, const T&)>>
-struct sparse_table{
-	vector<vector<T>> ST;
-  	F func;
+class SparseTable {
+ public:
+  int n;
+  vector<vector<T>> mat;
+  F func;
 
-	sparse_table(vector<T> &A){
-		int N = A.size();
-		int LOG = 32 - __builtin_clz(N);
-		ST = vector<vector<T>>(LOG, vector<T>(N));
-		for (int i = 0; i < N; i++){
-			ST[0][i] = A[i];
-		}
-		for (int i = 0; i < LOG - 1; i++){
-			for (int j = 0; j < N - (1 << i); j++){
-				ST[i + 1][j] = func(ST[i][j], ST[i][j + (1 << i)]);
-			}
-		}
-	}
-	T query(int L, int R){ // [L, R)
-		int d = 31 - __builtin_clz(R - L);
-		return func(ST[d][L], ST[d][R - (1 << d)]);
-	}
+  SparseTable(const vector<T>& a, const F& f) : func(f) {
+    n = static_cast<int>(a.size());
+    int max_log = 32 - __builtin_clz(n);
+    mat.resize(max_log);
+    mat[0] = a;
+    for (int j = 1; j < max_log; j++) {
+      mat[j].resize(n - (1 << j) + 1);
+      for (int i = 0; i <= n - (1 << j); i++) {
+        mat[j][i] = func(mat[j - 1][i], mat[j - 1][i + (1 << (j - 1))]);
+      }
+    }
+  }
+
+  T get(int from, int to) const { // [from, to]
+    assert(0 <= from && from <= to && to <= n - 1);
+    int lg = 32 - __builtin_clz(to - from + 1) - 1;
+    return func(mat[lg][from], mat[lg][to - (1 << lg) + 1]);
+  }
 };
 
 // link - https://codeforces.com/contest/1548/submission/229928645
