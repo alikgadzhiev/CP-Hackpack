5cb110519514ff84b978f1c62487f5fae36ebaf3
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2024-07-11 13:57:09 +0300

    Update Mint.cpp

--- Math/Mint.cpp
+++ Math/Mint.cpp
@@ -154,4 +154,17 @@ using Mint = Modular<VarMod>;
  
 constexpr int md = int(1e9) + 7;
 using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+vector<Mint> fact(1, 1);
+vector<Mint> inv_fact(1, 1);
  
+Mint C(int n, int k) {
+  if (k < 0 || k > n) {
+    return 0;
+  }
+  while ((int) fact.size() < n + 1) {
+    fact.push_back(fact.back() * (int) fact.size());
+    inv_fact.push_back(1 / fact.back());
+  }
+  return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
