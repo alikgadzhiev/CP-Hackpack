d246c87c29ab0ada7d3af694245cff73b01a3190
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2024-11-16 15:25:35 +0300

    Update manacher.cpp
    Signed-off-by: alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>

--- Strings/manacher.cpp
+++ Strings/manacher.cpp
@@ -1,51 +1,3 @@
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <functional>
-#include <utility>
-#include <bitset>
-#include <unordered_set>
-#include <cmath>
-#include <cstdlib>
-#include <random>
-#include <ctime>
-#include <chrono>
-#include <cstdio>
-
-using namespace std;
-
-template<typename T>
-istream &operator>>(istream &is, vector<T> &v) {
-    for (auto &i: v) is >> i;
-    return is;
-}
- 
-template<typename T>
-ostream &operator<<(ostream &os, vector<T> v) {
-    for (auto &i: v) os << i << ' ';
-    return os;
-}
- 
-long long mod = 998244353;
- 
-/**
- * manacher(S): return the maximum palindromic substring of S centered at each point
- *
- * Input: string (or vector) of length N (no restrictions on character-set)
- * Output: vector res of length 2*N+1
- *   For any 0 <= i <= 2*N:
- *   * i % 2 == res[i] % 2
- *   * the half-open substring S[(i-res[i])/2, (i+res[i])/2) is a palindrome of length res[i]
- *   * For odd palindromes, take odd i, and vice versa
- */
 template <typename V> std::vector<int> manacher(const V& S) {
 	int N = (int)S.size();
 	std::vector<int> res(2*N+1, 0);
@@ -73,7 +25,7 @@ int main() {
     string s;
     cin >> s;
     auto man = manacher(s);
-    auto is_pal = [&](int l, int r) -> bool {
+    auto is_pal = [&](int l, int r) -> bool { // [l, r)
         return r - l <= man[l + r];
     };
 }
