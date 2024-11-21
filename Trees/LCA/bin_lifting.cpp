3f91e6bcf79cfd03bdb8ef3b57c71fa2295cfcf5
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2024-11-21 21:27:02 +0300

    Update bin_lifting.cpp
    Signed-off-by: alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>

--- Trees/LCA/bin_lifting.cpp
+++ Trees/LCA/bin_lifting.cpp
@@ -2,57 +2,92 @@
 
 using namespace std;
 
-struct TreeAncestor{
-    vector<vector<int>> up;
-    vector<int> depth;
-    const int LOG = 20;
-public:
-    TreeAncestor(int n, vector<vector<int>> adj){
-        up = vector<vector<int>> (n, vector<int>(LOG));
-        depth = vector<int> (n);
-
-        function<void(int, int)> dfs = [&](int u, int pre){
-            for(int v : adj[u]){
-                if(v == pre)
-                    continue;
-                up[v][0] = u;
-                depth[v] = depth[u] + 1;
-                for(int j = 1; j < LOG; j++)
-                    up[v][j] = up[up[v][j - 1]][j - 1];
-                dfs(v, u);
-            }
-        };
-
-        dfs(0, -1);
+struct lca_lift {
+  const int lg = 24;
+  int n;
+  vector<int> depth;
+  vector<vector<int> > edges;
+  vector<vector<int> > lift;
+  
+  void init(int sz) {
+    n = sz;
+    depth = vector<int>(n);
+    edges = vector<vector<int> >(n, vector<int>());
+    lift = vector<vector<int> >(n, vector<int>(lg, -1));
+  }
+ 
+  void edge(int a, int b) {
+    edges[a].push_back(b);
+    edges[b].push_back(a);
+  }
+ 
+  void attach(int node_to_attach, int node_to_attach_to) {
+    int a = node_to_attach, b = node_to_attach_to;
+    edge(a, b);
+ 
+    lift[a][0] = b;
+ 
+    for (int i = 1; i < lg; i++) {
+      if (lift[a][i - 1] == -1) lift[a][i] = -1;
+      else lift[a][i] = lift[lift[a][i - 1]][i - 1];
     }
-
-    int getKthAncestor(int node, int k){
-        if(depth[node] < k){
-            return -1;
-        }
-        for(int j = LOG - 1; j >= 0; j--){
-            if(k & (1 << j))
-                node = up[node][j];
-        }
-        return node;
+ 
+    depth[a] = depth[b] + 1;
+  }
+ 
+  void init_lift(int v = 0) {
+    depth[v] = 0;
+    dfs(v, -1);
+  }
+ 
+  void dfs(int v, int par) {
+    lift[v][0] = par;
+ 
+    for (int i = 1; i < lg; i++) {
+      if (lift[v][i - 1] == -1) lift[v][i] = -1;
+      else lift[v][i] = lift[lift[v][i - 1]][i - 1];
     }
-
-    int getLCA(int a, int b){
-        if(depth[a] < depth[b]) {
-            swap(a, b);
-        }
-        a = getKthAncestor(a, depth[a] - depth[b]);
-        if(a == b) {
-            return a;
-        }
-        for(int j = LOG - 1; j >= 0; j--) {
-            if(up[a][j] != up[b][j]){
-                a = up[a][j];
-                b = up[b][j];
-            }
-        }
-        return up[a][0];
+ 
+    for (int x: edges[v]) {
+      if (x != par) {
+        depth[x] = depth[v] + 1;
+        dfs(x, v);
+      }
     }
+  }
+ 
+  int get(int v, int k) {
+    for (int i = lg - 1; i >= 0; i--) {
+	  if (v == -1) return v;
+      if ((1 << i) <= k) {
+        v = lift[v][i];
+        k -= (1 << i);
+      }
+    }
+    return v;
+  }
+ 
+  int get_lca(int a, int b) {
+    if (depth[a] < depth[b]) swap(a, b);
+    int d = depth[a] - depth[b];
+    int v = get(a, d);
+    if (v == b) {
+      return v;
+    } else {
+      for (int i = lg - 1; i >= 0; i--) {
+        if (lift[v][i] != lift[b][i]) {
+          v = lift[v][i];
+          b = lift[b][i];
+        }
+      }
+      return lift[b][0];
+    }
+  }
+  
+  int get_dist(int a, int b) {
+	  int v = get_lca(a, b);
+	  return depth[a] + depth[b] - 2 * depth[v];
+  }
 };
 
 int main(){
