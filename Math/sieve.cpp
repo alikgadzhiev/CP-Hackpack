46f9e648e5a5a2a1eb2c8ba49d32e121cb833f73
alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>
2025-08-16 12:32:11 +0300

    Create sieve.cpp
    Signed-off-by: alikgadzhiev <104996283+alikgadzhiev@users.noreply.github.com>

---
+++ Math/sieve.cpp
@@ -0,0 +1,28 @@
+std::vector<int> minp, primes, phi;
+ 
+void sieve(int n) {
+    minp.assign(n + 1, 0);
+    phi.assign(n + 1, 0);
+    primes.clear();
+    phi[1] = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        if (minp[i] == 0) {
+            minp[i] = i;
+            phi[i] = i - 1;
+            primes.push_back(i);
+        }
+        
+        for (auto p : primes) {
+            if (i * p > n) {
+                break;
+            }
+            minp[i * p] = p;
+            if (p == minp[i]) {
+                phi[i * p] = phi[i] * p;
+                break;
+            }
+            phi[i * p] = phi[i] * (p - 1);
+        }
+    }
+}
